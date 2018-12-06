dcb4abea2eb5dad6c4c85f44d34e388dfef738bc
szluis <szluis@outlook.com>
2018-12-06 15:41:37 -0400

    deployed

---
+++ build/precache-manifest.871e28314fc67b2bac92b63332c6d8ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f28b8df719840b6c12c0",
+    "url": "/static/js/main.f28b8df7.chunk.js"
+  },
+  {
+    "revision": "1b853021777142151471",
+    "url": "/static/js/1.1b853021.chunk.js"
+  },
+  {
+    "revision": "f28b8df719840b6c12c0",
+    "url": "/static/css/main.b178819c.chunk.css"
+  },
+  {
+    "revision": "1c64c4e8fcd66b42bd77691af5b2b227",
+    "url": "/index.html"
+  }
+];
