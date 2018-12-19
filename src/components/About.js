95c2f2232efb86e023cc28613aef43877a804645
szluis <szluis@outlook.com>
2018-12-19 18:14:53 -0400

    Updates

--- src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class About extends Component {
-    render() {
-        return (    <p>I'm a software developer</p>        )
-    }
-}
-
-export default About;
