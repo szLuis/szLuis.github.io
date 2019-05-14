18515b08d55923158434cb1368ace7624f715ace
Luis Salazar <szluis@outlook.com>
2019-05-14 11:19:46 -0400

    Updates

---
+++ precache-manifest.254c7f24c52a1bdbb60743c3c6757eb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b73d5e78545e35613719",
+    "url": "/static/js/main.b73d5e78.chunk.js"
+  },
+  {
+    "revision": "056b12cbe77d31a43bbf",
+    "url": "/static/js/1.056b12cb.chunk.js"
+  },
+  {
+    "revision": "b73d5e78545e35613719",
+    "url": "/static/css/main.ebbc7f3e.chunk.css"
+  },
+  {
+    "revision": "0db9b5e7d42eb5cec2fb2d52219c2adc",
+    "url": "/index.html"
+  }
+];
